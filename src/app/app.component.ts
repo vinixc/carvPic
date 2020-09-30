f88166f4b007e989ed616b85c64a6b01229dac1b
Vinicius Carvalho <vinicius.tecbcc@gmail.com>
2020-09-29 21:57:23 -0300

    diretiva

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  photos = [
+
+    {
+      url : 'https://kanto.legiaodosherois.com.br/w760-h398-gnw-cfill-q80/wp-content/uploads/2020/09/legiao_8e2uoBnN5EZF.jpg.jpeg',
+      description : 'Gohan'
+    },
+    {
+      url : 'https://www.imagenswiki.com/Uploads/imagenswiki.com/ImagensGrandes/son-goku-super-guerreiro.jpg',
+      description : 'Goku'
+    }
+  ]
 }
