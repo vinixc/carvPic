795ce2c89ef3475066b57fa492524ca2805e1881
Vinicius Carvalho <vinicius.tecbcc@gmail.com>
2020-09-29 21:15:09 -0300

    criando o component photo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'CarvPic';
-  description ='Gohan';
-  url = 'https://kanto.legiaodosherois.com.br/w760-h398-gnw-cfill-q80/wp-content/uploads/2020/09/legiao_8e2uoBnN5EZF.jpg.jpeg';
 }
---
+++ src/app/photo/photo.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'ap-photo',
+    templateUrl: 'photo.component.html'
+})
+export class PhotoComponent{
+
+    description ='Gohan';
+    
+    url = 'https://kanto.legiaodosherois.com.br/w760-h398-gnw-cfill-q80/wp-content/uploads/2020/09/legiao_8e2uoBnN5EZF.jpg.jpeg';
+
+}
