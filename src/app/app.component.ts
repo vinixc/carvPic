e333b691bb98336c92b53d5e03346db1bb4f05df
Vinicius Carvalho <vinicius.tecbcc@gmail.com>
2020-09-29 22:18:18 -0300

    HttpClient e injeção de dependência

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,15 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  photos = [
+  photos = [];
 
-    {
-      url : 'https://kanto.legiaodosherois.com.br/w760-h398-gnw-cfill-q80/wp-content/uploads/2020/09/legiao_8e2uoBnN5EZF.jpg.jpeg',
-      description : 'Gohan'
-    },
-    {
-      url : 'https://www.imagenswiki.com/Uploads/imagenswiki.com/ImagensGrandes/son-goku-super-guerreiro.jpg',
-      description : 'Goku'
-    }
-  ]
+  constructor(http: HttpClient){
+    console.log(http);
+  }
 }
