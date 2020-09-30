f4107f49dea13ed5c9cf005be4d4dcfd470dca1e
Vinicius Carvalho <vinicius.tecbcc@gmail.com>
2020-09-29 22:38:02 -0300

    Isolando a acesso em serviços

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { PhotoService } from './photos/photo/photo.service';
 
 @Component({
   selector: 'app-root',
@@ -10,12 +10,11 @@ export class AppComponent {
 
   photos : Object[] = [];
 
-  constructor(http: HttpClient){
+  constructor(photoService : PhotoService){
 
-    http
-      .get<Object[]>('http://localhost:3000/flavio/photos')
-      .subscribe(
-        photos => this.photos = photos);
+    photoService 
+      .listFromUser('flavio')
+      .subscribe(photos => this.photos = photos);
 
   }
 }
