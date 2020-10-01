6b7daa32ec84bf42c44dd47f3b429beae0c5121f
Vinicius Carvalho <vinicius.tecbcc@gmail.com>
2020-09-30 22:04:56 -0300

    Single Page Application e Rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
+import { PhotosModule } from './photos/photos.module';
+import { AppRoutingModule } from './app.rounting.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
-  
+    BrowserModule,
+    PhotosModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from '@angular/core';
 
 import { PhotoComponent } from './photo/photo.component';
 import { PhotoListComponent } from './photo-list/photo-list.component';
+import { PhotoFormComponent } from './photo-form/photo-form.component';
 
 @NgModule({
-    declarations : [ PhotoComponent, PhotoListComponent ],
+    declarations : [ PhotoComponent, PhotoListComponent, PhotoFormComponent ],
     imports : [ 
         HttpClientModule,
         CommonModule
