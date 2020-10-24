a810b648c781925cd28af1cbf68901503299cefe
Vinicius Carvalho <vinicius.tecbcc@gmail.com>
2020-10-24 19:09:08 -0300

    Acesso do header de resposta

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { tap } from 'rxjs/operators';
 
 const API_URL = 'http://localhost:3000';
 
@@ -12,6 +13,14 @@ export class AuthService {
 
   authenticate(userName:string,password:string){
     
-    return this.http.post(API_URL + '/user/login', {userName,password})
+    return this.http
+      .post(
+        API_URL + '/user/login',
+         {userName,password},
+         { observe : 'response'})
+      .pipe(tap(res => {
+        const authToken = res.headers.get('x-access-token');
+        console.log(authToken);
+      }))
   }
 }
