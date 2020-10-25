dc17eb3624c468c38e8aa1782bc87c54181eec6d
Vinicius Carvalho <vinicius.tecbcc@gmail.com>
2020-10-24 21:29:15 -0300

    Criando nosso próprio validador

--- src/app/home/singup/singup.component.ts
+++ src/app/home/singup/singup.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { lowerCaseValidator } from "src/app/shared/validators/lower-case.validator";
 
 @Component({
     templateUrl: './singup.component.html'
@@ -29,7 +30,7 @@ export class SingUpComponent implements OnInit{
             userName:['',
                 [
                     Validators.required,
-                    Validators.pattern(/^[a-z0-9_\-]+$/), 
+                    lowerCaseValidator, 
                     Validators.minLength(2),
                     Validators.maxLength(30)
                 ]
